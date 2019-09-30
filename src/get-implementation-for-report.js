20a79d2ed7d99a9f4e78d6178564a7115003ebd5
jkodu <jey.nandakumar@gmail.com>
2019-09-30 11:03:53 +0100

    migrate cmapper code from ACT Rules website

---
+++ src/get-implementation-for-report.js
@@ -0,0 +1,28 @@
+const getAssertions = require('./get-assertions')
+const getTestcasesGroupedByRule = require('./get-testcases-grouped-by-rule')
+const getRuleMapping = require('./get-rule-mapping')
+
+/**
+ * Get implementation metric from submitted report
+ *
+ * @param {Object|Array<Objects>} reports implementation reports
+ * @param {Object} testcases ACT Rules testcases
+ */
+const getImplementationForReport = async (reports, testcases) => {
+	const assertions = getAssertions(reports)
+	const testcasesGroupedByRuleId = await getTestcasesGroupedByRule(testcases)
+
+	return Object.keys(testcasesGroupedByRuleId)
+		.map(ruleId => {
+			const ruleTestcases = testcasesGroupedByRuleId[ruleId]
+			const implementation = getRuleMapping(ruleTestcases, assertions)
+			return {
+				ruleId,
+				ruleName: ruleTestcases[0].ruleName,
+				implementation,
+			}
+		})
+		.filter(result => result.implementation && result.implementation.length)
+}
+
+module.exports = getImplementationForReport
