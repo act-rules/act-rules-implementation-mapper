99195de7ea1f490f778aca6988511c1bf007f489
jkodu <jey.nandakumar@gmail.com>
2019-10-02 15:08:11 +0100

    update getFramedReports function

--- src/__tests__/json-loader.js
+++ src/__tests__/json-loader.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
 const createFiles = require('../../test-utils/create-files')
-const earlLoader = require('../json-loader')
+const jsonLoader = require('../json-loader')
 
 /**
  * temporary files created and cleaned up for tests
@@ -21,10 +21,10 @@ describe(`jsonLoader`, () => {
 	})
 
 	it(`throws when no argument is specified`, async () => {
-		const earlLoader = require('../json-loader')
+		const jsonLoader = require('../json-loader')
 		let error
 		try {
-			await earlLoader()
+			await jsonLoader()
 		} catch (e) {
 			error = e
 		}
@@ -32,38 +32,27 @@ describe(`jsonLoader`, () => {
 	})
 
 	it('returns json data from a given url', async () => {
-		const actual = await earlLoader(
+		const actual = await jsonLoader(
 			'https://raw.githubusercontent.com/act-rules/act-rules-implementation-alfa/master/report.json'
 		)
 		expect(actual).toBeArray()
 	})
 
 	it('returns json data from a single file path specified', async () => {
-		const actual = await earlLoader(`${__dirname}/tmp/file1.json`)
+		const actual = await jsonLoader(`${__dirname}/tmp/file1.json`)
 		expect(actual).toEqual([{ organisation: 'Deque' }])
 	})
 
 	it('returns combined json data from multiple file paths specified', async () => {
-		const actual = await earlLoader([`${__dirname}/tmp/file1.json`, `${__dirname}/tmp/file2.json`])
+		const actual = await jsonLoader([`${__dirname}/tmp/file1.json`, `${__dirname}/tmp/file2.json`])
 		expect(actual).toBeArrayOfSize(2)
 		expect(actual).toEqual([{ organisation: 'Deque' }, { organisation: 'Siteimprove' }])
 	})
 
 	it('returns combined json data from all files in a glob specified', async () => {
-		const actual = await earlLoader(`${__dirname}/tmp/*.json`)
+		const actual = await jsonLoader(`${__dirname}/tmp/*.json`)
 
 		expect(actual).toBeArrayOfSize(2)
 		expect(actual).toEqual([{ organisation: 'Deque' }, { organisation: 'Siteimprove' }])
 	})
-
-	// it('returns report when a multiple file paths are specified', async () => {
-	// 	require('fs').__createMockFiles(mockFiles)
-	// 	const earlLoader = require('../json-loader')
-	// 	const actual = await earlLoader(['/dir/file1.json', '/dir/file2.json'])
-
-	// 	expect(actual).toBeArray()
-	// 	expect(actual).toBeArrayOfSize(2)
-	// 	expect(actual[0]).toContainEntry(['organisation', 'Deque'])
-	// 	expect(actual[1]).toContainEntry(['organisation', 'Siteimprove'])
-	// })
 })
