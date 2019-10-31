3657601213ffde82ef8fdb1446f8316a0b96b4d8
Wilco Fiers <wilco.fiers@deque.com>
2019-10-31 13:58:54 +0100

    chore: fix minor code issues

--- src/get-implementation-set.ts
+++ src/get-implementation-set.ts
@@ -7,56 +7,56 @@ import { isConsistentImplementationSet } from './utils/is-consistent-implementat
 import { sortImplementations } from './utils/sort-implementations'
 
 export function getImplementationSet(assertions: EarlAssertion[], testcases: PartialTestcase[]) {
-	// Group asserts by implementation ID
-	const assertsByImplementationId = assertions.reduce((assertsByRule: AssertionGroup, assertion) => {
-		// Normalise around a bug in WCAG-EM
-		const test = assertion.test || assertion['wcagem-test']
-		if (!test) {
-			debug('getImplementationSet')(`Skipped! Could not find 'test' in assertion ${JSON.stringify(assertion)}`)
-			return assertsByRule
-		}
-		const implementationId = implementationIdFromTest(test)
-		if (!assertsByRule[implementationId]) {
-			assertsByRule[implementationId] = []
-		}
-		assertsByRule[implementationId].push(assertion)
-		return assertsByRule
-	}, {})
-	// Get all implementnations, sorted and with their ID added in
-	const allImplementations: Implementation[] = Object.entries(assertsByImplementationId)
-		.map(([implementationId, asssertions]) => ({
-			implementationId,
-			...getImplementation(testcases, asssertions),
-		}))
-		.sort(sortImplementations)
+  // Group asserts by implementation ID
+  const assertsByImplementationId = assertions.reduce((assertsByRule: AssertionGroup, assertion) => {
+    // Normalise around a bug in WCAG-EM
+    const test = assertion.test || assertion['wcagem-test']
+    if (!test) {
+      debug('getImplementationSet')(`Skipped! Could not find 'test' in assertion ${JSON.stringify(assertion)}`)
+      return assertsByRule
+    }
+    const implementationId = implementationIdFromTest(test)
+    if (!assertsByRule[implementationId]) {
+      assertsByRule[implementationId] = []
+    }
+    assertsByRule[implementationId].push(assertion)
+    return assertsByRule
+  }, {})
+  // Get all implementnations, sorted and with their ID added in
+  const allImplementations: Implementation[] = Object.entries(assertsByImplementationId)
+    .map(([implementationId, asssertions]) => ({
+      implementationId,
+      ...getImplementation(testcases, asssertions),
+    }))
+    .sort(sortImplementations)
 
-	// Figure out what implementations make for what consistency
-	const { implementations, consistency } = findConsistency(allImplementations)
-	const complete = implementations.every(({ complete }) => complete)
+  // Figure out what implementations make for what consistency
+  const { implementations, consistency } = findConsistency(allImplementations)
+  const complete = implementations.every(({ complete }) => complete)
 
-	return { complete, consistency, implementations }
+  return { complete, consistency, implementations }
 }
 
 function findConsistency(
-	implementations: Implementation[]
+  implementations: Implementation[]
 ): { consistency: Consistency; implementations: Implementation[] } {
-	const consistentImpl = implementations.filter(({ consistency }) => consistency === 'consistent')
-	const partialImpl = implementations.filter(({ consistency }) => consistency === 'partially-consistent')
-	if (consistentImpl.length > 0) {
-		return {
-			consistency: 'consistent',
-			implementations: consistentImpl,
-		}
-	}
-	if (partialImpl.length > 0) {
-		const consistency = isConsistentImplementationSet(partialImpl) ? 'consistent' : 'partially-consistent'
-		return {
-			consistency,
-			implementations: partialImpl,
-		}
-	}
-	return {
-		consistency: 'inconsistent',
-		implementations,
-	}
+  const consistentImpl = implementations.filter(({ consistency }) => consistency === 'consistent')
+  if (consistentImpl.length > 0) {
+    return {
+      consistency: 'consistent',
+      implementations: consistentImpl,
+    }
+  }
+  const partialImpl = implementations.filter(({ consistency }) => consistency === 'partially-consistent')
+  if (partialImpl.length > 0) {
+    const consistency = isConsistentImplementationSet(partialImpl) ? 'consistent' : 'partially-consistent'
+    return {
+      consistency,
+      implementations: partialImpl,
+    }
+  }
+  return {
+    consistency: 'inconsistent',
+    implementations,
+  }
 }
